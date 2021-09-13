b20b9770b0cc147590d60a817f8dd87b7781713c
learnwithkawsar <kawsar721g@gmail.com>
2021-09-13 22:46:39 +0600

    title change

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,10 +9,11 @@ const countDown =()=>{
     const day = hour *24;
 
     //calculate
-    const textDay = Math.floor((gap/day));
+    const textDay = Math.floor(gap/day);
     const textHour = Math.floor((gap%day)/hour);
     const textMinut = Math.floor((gap%hour)/minute);
     const textSecond = Math.floor((gap%minute)/second);
+
     document.querySelector('.day').innerText = textDay<10?'0'+textDay:textDay;
     document.querySelector('.hour').innerText = textHour<10?'0'+textHour:textHour;
     document.querySelector('.minute').innerText = textMinut<10?'0'+textMinut:textMinut;
